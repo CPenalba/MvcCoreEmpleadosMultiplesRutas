fc3b8ebdb836d0c0034a05f502c10185589416f8
CPenalba <carolinapenalbacorpa@gmail.com>
2025-03-24 11:25:29 +0100

    Add project files.

---
+++ MvcCoreEmpleadosMultiplesRutas/Controllers/EmpleadosController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosMultiplesRutas.Services;
+using NugetApiModelsCPC;
+
+namespace MvcCoreEmpleadosMultiplesRutas.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private ServiceEmpleados service;
+
+        public EmpleadosController(ServiceEmpleados service)
+        {
+            this.service = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<Empleado> empleados = await this.service.GetEmpleadosAsync();
+            List<string> oficios = await this.service.GetOficiosAsync();
+            ViewData["OFICIOS"] = oficios;
+            return View(empleados);
+        }
+    }
+}
